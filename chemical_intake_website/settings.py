4b89f9159885fd066d83358944fb80b9e3a45034
Asmit Paul <hiasmitpaul@gmail.com>
2025-06-14 14:03:28 +0530

    added git ignore

--- chemical_intake_website/settings.py
+++ chemical_intake_website/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -121,7 +122,7 @@ USE_TZ = True
 STATIC_URL = "static/"
 
 STATICFILES_DIRS = [
-    "/static/",
+    BASE_DIR / "static",
 ]
 
 # Default primary key field type
